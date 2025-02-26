12187b0af350f7b488cd693591aceda5a8ec29f8
Mikael Larsson <s.mikael.larsson@gmail.com>
2025-02-26 14:14:28 +0100

    skapa modell för sånger

--- Models/SongModel.cs
+++ Models/SongModel.cs
@@ -1,5 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Moment4Api.Models;
 
-public class SongModel {
-    
+public class SongModel
+{
+    //props
+    public int SongId { get; set; }
+
+    [Required]
+    public string? Artist { get; set; }
+
+    [Required]
+    public string? Title { get; set; }
+
+    public int Length { get; set; }
+
+    public string? Category { get; set; }
 }
